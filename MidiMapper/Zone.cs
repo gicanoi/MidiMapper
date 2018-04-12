0f5073e56cad4e7ebed7b6a1564d03c4e22a079a
Nacho Fernández <gicanoi@gmail.com>
2018-04-12 01:53:01 -0300

    first commit

---
+++ MidiMapper/Zone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MidiMapper
+{
+    public class Zone
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public int LowerNote { get; set; }
+        public int HigherNote { get; set; }
+
+        /// <summary>
+        /// null for Omni
+        /// </summary>
+        public int? InputChannel { get; set; }
+
+        public int Transpose { get; set; }
+
+
+    }
+}
